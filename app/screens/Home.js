9c158762f56cdf3332a6b24772bd773be2ee6abd
Rhys Walden <rhys.walden.dev@gmail.com>
2017-10-21 13:22:53 +1300

    remove image url

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -2,33 +2,6 @@ import React from "react";
 import { ScrollView, Text, Linking, View } from "react-native";
 import { Card, Button } from "native-base";
 
-const images = [
-  {
-    key: 1,
-    name: "Nathan Anderson",
-    image: require("../images/1.jpg"),
-    url: "https://unsplash.com/photos/C9t94JC4_L8"
-  },
-  {
-    key: 2,
-    name: "Jamison McAndie",
-    image: require("../images/2.jpg"),
-    url: "https://unsplash.com/photos/waZEHLRP98s"
-  },
-  {
-    key: 3,
-    name: "Alberto Restifo",
-    image: require("../images/3.jpg"),
-    url: "https://unsplash.com/photos/cFplR9ZGnAk"
-  },
-  {
-    key: 4,
-    name: "John Towner",
-    image: require("../images/4.jpg"),
-    url: "https://unsplash.com/photos/89PFnHKg8HE"
-  }
-];
-
 export default () => (
   <View style={{ flex: 1 }}>
 
