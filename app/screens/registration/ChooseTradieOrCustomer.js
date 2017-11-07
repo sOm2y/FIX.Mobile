3f4539aa91a14c9e77af9666a9a65cbdfe7654cf
dev.yuey <Yue.Yin@asb.co.nz>
2017-11-07 16:13:12 +1300

    router setup

---
+++ app/screens/registration/ChooseTradieOrCustomer.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { StyleSheet } from 'react-native';
+import { translate } from 'react-i18next';
+import { Container,Header, Body, Title, Content, List, ListItem, Button, Text, Left, Right, Icon } from "native-base";
+import { Confirmation } from './Confirmation';
+
+
+@translate(['home', 'common'], { wait: true })
+
+export default class ChooseTradieOrCustomer extends React.Component {
+  static navigationOptions = ({ navigation, screenProps }) => ({
+    title: screenProps.t('home:title')
+  });
+
+  render(){
+
+    const { t, i18n, navigation } = this.props;
+    const { navigate } = navigation;
+
+    return (
+      <Container>
+        <Header>
+        <Left>
+            <Button transparent onPress={() => navigation.navigate("SignIn")}>
+                <Icon name="arrow-back" />
+            </Button>
+            </Left>
+          <Body>
+            <Title>Address</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+            <Button full rounded primary
+                style={{ marginTop: 10 }}
+                onPress={() => navigation.navigate("PersonalDetail")}>
+            <Text>Goto Confirmation</Text>
+            </Button>
+            <Button full rounded primary
+                style={{ marginTop: 10 }}
+                onPress={() => navigation.navigate("PersonalDetail")}>
+            <Text>Goto Confirmation</Text>
+          </Button>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+//TODO: Bug from nativebase
+const styles = StyleSheet.create({
+  listItem:{
+    marginLeft: 0, 
+    paddingLeft: 17
+  }
+ 
+});
