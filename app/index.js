36e010b6273e2b8e4386917d7870db3923e59b01
dev.yuey <Yue.Yin@asb.co.nz>
2017-12-11 16:00:35 +1300

    finger print

--- app/index.js
+++ app/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Platform, Text, View, Alert } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Fingerprint } from 'expo';
 import { I18nextProvider, translate } from 'react-i18next';
 import { Root } from "native-base";
 import { createRootNavigator } from "./router";
@@ -11,23 +14,50 @@ export default class Index extends React.Component {
     super(props);
 
     this.state = {
+      hasFingerprintAuth: null,
       signedIn: false,
       checkedSignIn: false
     };
   }
 
   componentWillMount() {
-    isSignedIn()
-      .then(res => this.setState({ signedIn: res, checkedSignIn: true }))
-      .catch(err => {
-        Toast.show({
-          text: err,
-          type: "success",
-          buttonText: "Dismiss",
-          duration: 3000
-         }); 
-      });
+    Fingerprint.hasHardwareAsync().then(hasHardware => {
+      hasHardware &&
+        Fingerprint.isEnrolledAsync().then(hasFingerprintAuth => {
+          this.setState({ hasFingerprintAuth });
+          if( Platform.OS === 'android'){
+            Alert.alert('Place your finger to scan.')
+          }
+          this.authFunction();
+        });
+    });
+  
   }
+
+  authFunction = async () => {
+    try {
+      let result =
+        Platform.OS === 'ios'
+          ? await Fingerprint.authenticateAsync('Show me your finger')
+          : await Fingerprint.authenticateAsync();
+
+      if (result.success) {
+          isSignedIn()
+          .then(res => this.setState({ signedIn: result.success, checkedSignIn: result.success }));
+      } else {
+        this.setState({ signedIn: result.success, checkedSignIn: result.success });
+        console.log('Fingerprint Auth Failed', result);
+      }
+    } catch (err) {
+      console.log('authFunction Error', err);
+    }
+  };
+
+  getAuthStatement = () => {
+    return (
+      Alert.alert('Place your finger to scan.')
+    );
+  };
 
   render() {
     const { checkedSignIn, signedIn } = this.state;
@@ -40,7 +70,9 @@ export default class Index extends React.Component {
     const RootLayout = createRootNavigator(signedIn);
 
     const WrappedStack = () => {
-      return <RootLayout screenProps={{ t: i18n.getFixedT() }} />;
+      return (
+        <RootLayout screenProps={{ t: i18n.getFixedT() }}/> 
+        );
     }
 
     const ReloadAppOnLanguageChange = translate('common', {
@@ -52,6 +84,7 @@ export default class Index extends React.Component {
       <Root>
         <I18nextProvider i18n={ i18n }>
           <ReloadAppOnLanguageChange />
+        
         </I18nextProvider>
       </Root>
     );
