0000fe2b8099716217afafc9610a2f910d6474f7
dev.yuey <Yue.Yin@asb.co.nz>
2018-01-08 14:21:22 +1300

    add picker input

---
+++ app/components/inputs/renderPicker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Picker } from 'native-base';
+
+export const renderName= ({ input, label, children, ...custom }) => {
+    
+    return ( 
+        <Picker placeholder="Select One" {...input} selectedValue={input.value} onValueChange={(value, index) => input.onChange(value)} {...custom}> 
+            { children }
+        </Picker> );
+};
