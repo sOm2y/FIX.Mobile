6e7d39615a0844f69f29c64526b679270e1ddbef
Yue Yin <samyin1990@gmail.com>
2018-04-11 11:16:01 +1200

    fix navigation load screen twice, fix navigation doesn't work

--- app/navigations/navigationStack.js
+++ app/navigations/navigationStack.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { AsyncStorage, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { StackNavigator, TabNavigator, addNavigationHelpers } from 'react-navigation';
-import { Button, Text, Icon, Footer, FooterTab } from "native-base";
+import {
+  StackNavigator,
+  TabNavigator,
+  addNavigationHelpers
+} from 'react-navigation';
+import { Button, Text, Icon, Footer, FooterTab } from 'native-base';
 
 import SignUpScreen from '../screens/SignUpScreen';
 import SignIn from '../screens/SignInScreen';
@@ -18,105 +22,106 @@ import ProfileScreen from '../screens/ProfileScreen';
 import FooterBottomTab from '../components/FooterBottomTab';
 import JobDetailScreen from '../screens/jobs/JobDetailScreen';
 
-export const JobNavigator = StackNavigator(
-  { 
-    Jobs:{
-      screen: JobsScreen,
-      navigationOptions: {
-        
-      }
-    },
-    JobDetail:{
-      screen: JobDetailScreen,
-      navigationOptions: {
-        
-      }
-    },
-    CreateJob:{
-      screen: CreateJobScreen,
-      navigationOptions: {
-        
-      }
-    },
-    TradieFinder:{
-      screen: TradieFinderScreen,
-      navigationOptions: {
-        
-      }
-    }
-  },{
-    headerMode: 'none',
-    animationEnabled: true,
-    gesturesEnabled:true,
-    initialRouteName : 'Jobs'
-  });
-   
-export const RegistrationNavigator = StackNavigator(
-  { 
-    SignUp:{
-      screen: SignUpScreen,
-      navigationOptions: {
-        
-      }
-    },
-    Customer:{
-      screen: CustomerRegister,
-      navigationOptions: {
-        
-      }
-    },
-    Tradie:{
-      screen: TradieRegister,
-      navigationOptions: {
-        
-      }
-    }
-  
-  },{
-    headerMode: 'none',
-    animationEnabled: true,
-    gesturesEnabled:true,
-    initialRouteName : 'SignUp',
-  });
+// export const JobNavigator = StackNavigator(
+//   {
+//     Jobs:{
+//       screen: JobsScreen,
+//       navigationOptions: {
 
-export const HomeTabNavigator = TabNavigator(
+//       }
+//     }
+//   },{
+//     headerMode: 'none',
+//     animationEnabled: true,
+//     gesturesEnabled:true,
+//     // initialRouteName : 'Jobs'
+//   });
+
+export const RegistrationNavigator = StackNavigator(
   {
-    Jobs: { 
-      screen: JobNavigator,
-      navigationOptions: {
-        
-      }
+    SignUp: {
+      screen: SignUpScreen,
+      navigationOptions: {}
     },
-    JobsOnMap:{
-      screen: JobsOnMap,
-      navigationOptions: {
-        
-      }
+    Customer: {
+      screen: CustomerRegister,
+      navigationOptions: {}
     },
-    Notifications: { 
-      screen: NotificationsScreen,
-      navigationOptions: {
-        
-      }
-    },
-    Profile: { 
-      screen: ProfileScreen,
-      navigationOptions: {
-        
-      }
+    Tradie: {
+      screen: TradieRegister,
+      navigationOptions: {}
     }
   },
   {
-    tabBarPosition: "bottom",
-    animationEnabled: false,
-    tabBarOptions: {
-     // activeTintColor: '#e91e63',
-    },
-    tabBarComponent: ({navigationState, navigation}) => <FooterBottomTab navigation={navigation} navigationState={navigationState} />
+    headerMode: 'none',
+    animationEnabled: true,
+    gesturesEnabled: true,
+    initialRouteName: 'SignUp'
   }
 );
 
-const AppRootNavigator =  StackNavigator(
+export const HomeTabNavigator = TabNavigator(
+  {
+    Jobs: {
+      screen: JobsScreen,
+      navigationOptions: {}
+    },
+    JobsOnMap: {
+      screen: JobsOnMap,
+      navigationOptions: {}
+    },
+    Notifications: {
+      screen: NotificationsScreen,
+      navigationOptions: {}
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {}
+    }
+  },
+  {
+    initialRouteName: 'Jobs',
+    tabBarPosition: 'bottom',
+    animationEnabled: false,
+    tabBarOptions: {
+      // activeTintColor: '#e91e63',
+    },
+    tabBarComponent: ({ navigationState, navigation }) => (
+      <FooterBottomTab
+        navigation={navigation}
+        navigationState={navigationState}
+      />
+    )
+  }
+);
+
+export const HomeStackNavigator = StackNavigator(
+  {
+    Jobs: {
+      screen: HomeTabNavigator,
+      navigationOptions: {}
+    }
+    ,
+    JobDetail: {
+      screen: JobDetailScreen,
+      navigationOptions: {}
+    },
+    CreateJob: {
+      screen: CreateJobScreen,
+      navigationOptions: {}
+    },
+    TradieFinder: {
+      screen: TradieFinderScreen,
+      navigationOptions: {}
+    }
+  },
+  {
+    headerMode: 'none',
+    initialRouteName: 'Jobs'
+  }
+);
+
+const AppRootNavigator = StackNavigator(
   {
     SignIn: {
       screen: SignIn,
@@ -131,9 +136,9 @@ const AppRootNavigator =  StackNavigator(
       }
     },
     Home: {
-      screen: HomeTabNavigator,
+      screen: HomeStackNavigator,
       navigationOptions: {
-        gesturesEnabled: false,
+        gesturesEnabled: false
       }
     }
   },
@@ -144,10 +149,9 @@ const AppRootNavigator =  StackNavigator(
 );
 
 const styles = StyleSheet.create({
-
   tabText: {
-    fontSize: 10,
-  },
+    fontSize: 10
+  }
 });
 
 export default AppRootNavigator;
